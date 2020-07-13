96d0ecf8ac1ca3dba978437c27551d18006c55cc
Dan <businessdaniel@live.com>
2020-07-13 19:05:33 -0400

    Started Game, Controls Complete.
    Just need to make logic for Ball and points.

---
+++ Grenade Brothers/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+    public float jumpVelocity = 5f;
+    
+    private bool isJumping = false;
+    private float movement;        
+
+    void Update() {
+        if (isPlayer1) {
+            movement = Input.GetAxisRaw("Horizontal");
+
+            if (Input.GetButtonDown("Jump") && !isJumping) { 
+                rb.velocity = Vector2.up * jumpVelocity;
+                isJumping = true;
+            }
+
+        } else { // Is Player 2
+            movement = Input.GetAxisRaw("Horizontal2"); 
+
+            if (Input.GetButtonDown("Jump2") && !isJumping) { 
+                rb.velocity = Vector2.up * jumpVelocity;
+                isJumping = true;
+            }
+        }
+
+        rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D (Collision2D col) {
+         if (col.gameObject.tag == "ground") {
+             isJumping = false;
+         }
+     }
+}
